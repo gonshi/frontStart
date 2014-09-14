303b99595ee84b3fe1f9253257c0a1c9922320ba
Tajima Shingo <tajima-shingo@kayac.com>
2014-09-14 16:34:05 +0900

    mod tab to space

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 (function(global, doc, $, ns, undefined) {
-	'use strict';
-	ns = ns || {};
+  'use strict';
+  ns = ns || {};
 
-	$(function() {
-	});
-	global.namespace = ns;
+  $(function() {
+  });
+  global.namespace = ns;
 })(this, document, jQuery, this.namespace);
