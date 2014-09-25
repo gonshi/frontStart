d86316a647d4dac2be9c5ba63479ea81b1d13606
Tajima Shingo <tajima-shingo@kayac.com>
2014-09-26 00:50:08 +0900

    change for in loop to for loop in eventDispatcher

--- src/js/util/EventDispatcher.js
+++ src/js/util/EventDispatcher.js
@@ -1,4 +1,4 @@
-(function(global, doc, $, ns, undefined) {
+(function( global, doc, $, ns, undefined ) {
   'use strict';
   ns = ns || {};  
 
@@ -6,59 +6,61 @@
     this._events = {};
   }
 
-  EventDispatcher.prototype.hasEventListener = function(eventName) {
-    return !!this._events[eventName];
+  EventDispatcher.prototype.hasEventListener = function( eventName ) {
+    return !!this._events[ eventName ];
   };
 
-  EventDispatcher.prototype.addEventListener = function(eventName, callback) {
-    if (this.hasEventListener(eventName)) {
-      var events = this._events[eventName];
-      for (var i in events) {
-        if (events[i] === callback) {
+  EventDispatcher.prototype.addEventListener = function( eventName, callback ) {
+    if ( this.hasEventListener(eventName) ) {
+      var events = this._events[ eventName ];
+      var i;
+      for ( i = 0; i < events.length; i++ ) {
+        if ( events[ i ] === callback ) {
           return;
         }
       }
-      events.push(callback);
+      events.push( callback );
     }
     else{
-      this._events[eventName] = [callback];
+      this._events[ eventName ] = [ callback ];
     }
     return this;
   };
 
-  EventDispatcher.prototype.removeEventListener = function(eventName, callback) {
-    if (!this.hasEventListener(eventName)) {
+  EventDispatcher.prototype.removeEventListener = function( eventName, callback ) {
+    if ( !this.hasEventListener(eventName) ) {
       return;
     }
     else{
-      var events = this._events[eventName],
+      var events = this._events[ eventName ],
           i      = events.length,
           index;
-      while (i--) {
-        if (events[i] === callback) {
+      while ( i-- ) {
+        if ( events[ i ] === callback ) {
           index = i;
         }
       }
-      events.splice(index, 1);
+      events.splice( index, 1 );
     }
     return this;
   };
 
-  EventDispatcher.prototype.fireEvent = function(eventName, opt_this) {
-    if (!this.hasEventListener(eventName)) {
+  EventDispatcher.prototype.fireEvent = function( eventName, opt_this ) {
+    if ( !this.hasEventListener(eventName) ) {
       return;
     }
     else{
-      var events = this._events[eventName],
-      copyEvents = $.merge([], events),
-      arg        = $.merge([], arguments);
-      arg.splice(0, 2);
-      for (var i in copyEvents) {
-        copyEvents[i].apply(opt_this || this, arg);
+      var events = this._events[ eventName ],
+      var i;
+      copyEvents = $.merge( [], events ),
+      arg        = $.merge( [], arguments );
+      arg.splice( 0, 2 );
+      for ( i = 0; i < copyEvents.length; i++ ) {
+        copyEvents[ i ].apply( opt_this || this, arg );
       }
     }
   };
 
   ns.EventDispatcher = EventDispatcher;
   global.namespace = ns;
-})(this, document, jQuery, this.namespace);
+})( this, document, jQuery, this.namespace );
